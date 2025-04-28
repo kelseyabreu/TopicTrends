3250dc1c6520fe16f0657c66308c6da74bc4f0ad
Jason <jasonc62492@gmail.com>
2025-04-27 22:09:43 -0400

    moved logg init into its own core file (#13)
    * moved logg init into its own core file
    
    * log core routers
    
    * log core routers

---
+++ backend/app/core/logging.py
@@ -0,0 +1,15 @@
+# app/core/logging.py or similar
+import logging
+import sys
+
+def setup_logger():
+    # Configure the root logger
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.StreamHandler(sys.stdout)
+        ]
+    )
+
+# Call this function at application startup
