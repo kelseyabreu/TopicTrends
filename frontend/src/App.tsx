fecd8e236a5a010ea58a15950f484955e9326357
Kelsey Abreu <Kelseyabreu@hotmail.com>
2025-04-28 17:26:30 -0400

    forgot passwords. Yay

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,8 @@ import AllDiscussionsView from './pages/AllDiscussionsView.tsx';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import VerifyEmail from './pages/VerifyEmail';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
@@ -16,32 +18,34 @@ import ProtectedRoute from './components/ProtectedRoute.tsx';
 import UserSettings from './pages/UserSettings.tsx';
 import IdeaView from './pages/IdeaView.tsx';
 
-function App() {  
+function App() {
     return (
         <Router>
-        <div className="App">
-            <Header />
-            <div className="container">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/verify" element={<VerifyEmail />} />
-                <Route path="/create" element={<CreateDiscussion />} />
-                <Route path="/ideas/:ideaId" element={<IdeaView />} />
-                <Route path="/join/:discussionId" element={<JoinDiscussion />} />
-                <Route path="/discussions" element={<AllDiscussionsView />} />
-                <Route path="/discussion/:discussionId" element={<DiscussionView />} />
-                <Route path="/discussion/:discussionId/topic/:topicId" element={<TopicView />} />
-                <Route
-                        path="/settings"
-                            element={
-                                <ProtectedRoute>
-                                    <UserSettings />
-                                </ProtectedRoute>
-                            }
-                        />
-                </Routes>
+            <div className="App">
+                <Header />
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/verify" element={<VerifyEmail />} />
+                        <Route path="/forgot-password" element={<ForgotPassword />} />
+                        <Route path="/reset-password" element={<ResetPassword />} />
+                        <Route path="/create" element={<CreateDiscussion />} />
+                        <Route path="/ideas/:ideaId" element={<IdeaView />} />
+                        <Route path="/join/:discussionId" element={<JoinDiscussion />} />
+                        <Route path="/discussions" element={<AllDiscussionsView />} />
+                        <Route path="/discussion/:discussionId" element={<DiscussionView />} />
+                        <Route path="/discussion/:discussionId/topic/:topicId" element={<TopicView />} />
+                        <Route
+                                path="/settings"
+                                    element={
+                                        <ProtectedRoute>
+                                            <UserSettings />
+                                        </ProtectedRoute>
+                                    }
+                                />
+                        </Routes>
             </div>
             <ToastContainer position="bottom-right" />
         </div>
