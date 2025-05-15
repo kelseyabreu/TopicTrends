7063c1f9173b71092af4c98c4f459016a37e8e27
Kelsey Abreu <Kelseyabreu@hotmail.com>
2025-05-15 15:06:49 -0400

    my ideas

--- frontend/src/interfaces/ideas.ts
+++ frontend/src/interfaces/ideas.ts
@@ -5,11 +5,20 @@ export interface Idea {
     user_id: string;
     verified: boolean;
     timestamp: string;
-    topic_id: string;
+    topic_id: string | null;
+    discussion_id: string;
     related_topics: string[];
     keywords: string[];
     on_topic: number;
     sentiment:string;
     specificity:string;
     intent: string;
+}
+export interface IdeaStats {
+    intentCounts: Record<string, number>;
+    sentimentCounts: Record<string, number>;
+    clusterRate: number;
+    totalIdeas: number;
+    clusteredIdeas: number;
+    discussionsCount: number;
 }
